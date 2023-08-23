8509feeb272b21e6e245b9c32c55ad1a71a24efd
evduanG <eviatar.duany@gmail.com>
2023-08-23 21:41:23 +0300

    fix bug /admin/login goto  dashboardwhan login

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -11,7 +11,7 @@ class AdminController extends Controller
 
     public function viewLogin()
     {
-        if (isset(Auth::guard('admin')->user->name)) {
+        if (Auth::guard('admin')->check()) {
             return redirect()->route('admins.dashboard');
         }
         return view('admin.login-admin');
